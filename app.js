20b2ef7728dbf0f5a786f5eae1fea65184358de2
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2022-08-17 18:18:14 +0100

    Stop sending the access token to the client

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+//
+// Type definitions
+//
+
+/**
+ * @typedef { import("express").Request } Request
+ */
+
+/**
+ * @typedef { import("express").Response } Response
+ */
+
+/**
+ * @typedef { import("express").NextFunction } NextFunction
+ */
+
+//
+// Imports
+//
+
 import "dotenv/config";
 import axios from "axios";
 import cookieParser from "cookie-parser";
 import express from "express";
 import { fileURLToPath } from "url";
+import nunjucks from "nunjucks";
 import { randomUUID } from "crypto";
+import session from "express-session";
 
 //
 // Constants
@@ -13,11 +35,67 @@ const __DIRNAME = fileURLToPath(new URL(".", import.meta.url));
 const STATE_KEY = "github_auth_state";
 
 const app = express();
+const sessionSettings = {
+  cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day
+  resave: false,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+};
 
 //
 // Functions
 //
 
+/**
+ * Handle the / route.
+ * @param {Request} req The Request object.
+ * @param {Response} res The Response object.
+ */
+function index(req, res) {
+  const { token } = req.session;
+
+  if (token) {
+    res.redirect("/repos");
+    return;
+  }
+
+  res.render("index");
+}
+
+/**
+ * Handle the /repos route.
+ * @param {Request} req The Request object.
+ * @param {Response} res The Response object.
+ */
+function repos(req, res) {
+  const { token } = req.session;
+
+  if (!token) {
+    res.redirect("/");
+    return;
+  }
+
+  const requestConfig = {
+    method: "get",
+    headers: { Authorization: `token ${token}` },
+    url: `https://api.github.com/user/repos`,
+  };
+
+  axios(requestConfig)
+    .then(function ({ data: repos }) {
+      res.render("repos", { repos });
+    })
+    .catch(function (error) {
+      console.log("Something went wrong:", error);
+      res.redirect("/");
+    });
+}
+
+/**
+ * Handle the /login route.
+ * @param {Request} _req The Request object.
+ * @param {Response} res The Response object.
+ */
 function login(_req, res) {
   const state = randomUUID();
   const params = new URLSearchParams({
@@ -31,7 +109,13 @@ function login(_req, res) {
     .redirect(`https://github.com/login/oauth/authorize?${params.toString()}`);
 }
 
-function callback(req, res) {
+/**
+ * Handle the /callback route.
+ * @param {Request} req The Request object.
+ * @param {Response} res The Response object.
+ * @param {NextFunction} next The next function in the request-response cycle.
+ */
+function callback(req, res, next) {
   const { code, state } = req.query;
   const storedState = req.cookies?.[STATE_KEY];
 
@@ -40,46 +124,71 @@ function callback(req, res) {
     return;
   }
 
-  const params = new URLSearchParams({
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
-    code,
-  });
+  // Regenerate the session, which is good practice to help
+  // guard against forms of session fixation.
+  req.session.regenerate((err) => {
+    if (err) next(err);
 
-  const requestConfig = {
-    method: "post",
-    headers: { Accept: "application/json" },
-    url: `https://github.com/login/oauth/access_token?${params.toString()}`,
-  };
-
-  res.clearCookie(STATE_KEY);
-
-  axios(requestConfig)
-    .then(function ({ data }) {
-      res.redirect(`/app?token=${data.access_token}`);
-    })
-    .catch(function (error) {
-      console.log("Something went wrong:", error);
-      res.redirect("/");
+    const params = new URLSearchParams({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code,
     });
-}
 
-function listen(error) {
-  if (error) {
-    console.log("Something went wrong:", error);
-    return;
-  }
+    const requestConfig = {
+      method: "post",
+      headers: { Accept: "application/json" },
+      url: `https://github.com/login/oauth/access_token?${params.toString()}`,
+    };
 
-  console.log(`Listening on port ${process.env.PORT}...`);
+    res.clearCookie(STATE_KEY);
+
+    axios(requestConfig)
+      .then(function ({ data: { access_token } }) {
+        // Store access token in session.
+        req.session.token = access_token;
+
+        // Save the session before redirection to ensure page
+        // load does not happen before session is saved.
+        req.session.save((err) => {
+          if (err) return next(err);
+          res.redirect("/repos");
+        });
+      })
+      .catch(function (err) {
+        console.log("Something went wrong:", err);
+        res.redirect("/");
+      });
+  });
 }
 
 //
 // Inits
 //
 
+// Template engine config
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+
+// Settings
+app.set("views", "./views").set("view engine", "njk");
+
+// Middleware
 app
   .use(express.static(`${__DIRNAME}/public`))
   .use(cookieParser())
+  .use(session(sessionSettings));
+
+// Routes
+app
+  .get("/", index)
+  .get("/repos", repos)
   .get("/login", login)
-  .get("/callback", callback)
-  .listen(process.env.PORT, listen);
+  .get("/callback", callback);
+
+// Listener
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}...`);
+});
--- public/globals.js
+++
@@ -1,2 +0,0 @@
-export const API = "https://api.github.com/user/repos?affiliation=owner";
-export const STORAGE_KEY = "gitclub_token";
